74edc64c01c8fc8e145d7d079cf5e9080a38ba79
Kanika Pasrija <kapasrij@microsoft.com>
2020-09-07 12:58:00 +0530

    replacing files

--- src/deploy/scope_managementgroup.ts
+++ src/deploy/scope_managementgroup.ts
@@ -1,22 +1,24 @@
 import { exec } from '@actions/exec';
 import { ExecOptions } from '@actions/exec/lib/interfaces';
 import { ParseOutputs, Outputs } from '../utils/utils';
-import { info, warning } from '@actions/core';
+import * as core from '@actions/core';
 
-export async function DeployManagementGroupScope(azPath: string, validationOnly: boolean, location: string,  templateLocation: string, deploymentMode: string, deploymentName: string, parameters: string, managementGroupId: string): Promise<Outputs> {    
-    // Check if location is set
-    if (!location) {
-        throw Error("Location must be set. zaruri hai.")
+export async function DeployManagementGroupScope(azPath: string, validationOnly: boolean, region: string, template: string, deploymentMode: string, deploymentName: string, parameters: string, managementGroupId: string): Promise<Outputs> {
+    // Check if region is set
+    if (!region) {
+        throw Error("Region must be set.")
     }
+
+    // check if mode is set as this will be ignored
     if (deploymentMode != "") {
-        warning("Deployment Mode to be ignored")
+        core.warning("Deployment Mode is not supported for subscription scoped deployments, this parameter will be ignored!")
     }
     // create the parameter list
     const azDeployParameters = [
-        location ? `--location ${location}` : undefined,
-        templateLocation ?
-            templateLocation.startsWith("http") ? `--template-uri ${templateLocation}`: `--template-file ${templateLocation}`
-        : undefined,
+        region ? `--location ${region}` : undefined,
+        template ?
+            template.startsWith("http") ? `--template-uri ${template}` : `--template-file ${template}`
+            : undefined,
         managementGroupId ? `--management-group-id ${managementGroupId}` : undefined,
         deploymentName ? `--name ${deploymentName}` : undefined,
         parameters ? `--parameters ${parameters}` : undefined
@@ -29,7 +31,7 @@ export async function DeployManagementGroupScope(azPath: string, validationOnly:
         failOnStdErr: true,
         listeners: {
             stderr: (data: BufferSource) => {
-                warning(data.toString());
+                core.warning(data.toString());
             },
             stdline: (data: string) => {
                 if (!data.startsWith("[command]"))
@@ -43,25 +45,26 @@ export async function DeployManagementGroupScope(azPath: string, validationOnly:
         ignoreReturnCode: true,
         listeners: {
             stderr: (data: BufferSource) => {
-                warning(data.toString());
+                core.warning(data.toString());
             },
         }
     }
 
     // validate the deployment
-    info("Validating template...")
+    core.info("Validating template...")
     var code = await exec(`"${azPath}" deployment mg validate ${azDeployParameters} -o json`, [], validateOptions);
     if (validationOnly && code != 0) {
         throw new Error("Template validation failed")
     } else if (code != 0) {
-        warning("Template validation failed.")
+        core.warning("Template validation failed.")
     }
 
     // execute the deployment
-    info("Creating deployment...")
+    core.info("Creating deployment...")
     await exec(`"${azPath}" deployment mg create ${azDeployParameters} -o json`, [], deployOptions);
-
+    core.debug(commandOutput);
+    
     // Parse the Outputs
-    info("Parsing outputs...")
+    core.info("Parsing outputs...")
     return ParseOutputs(commandOutput)
 }
--- src/deploy/scope_subscription.ts
+++ src/deploy/scope_subscription.ts
@@ -1,24 +1,24 @@
 import { exec } from '@actions/exec';
 import { ExecOptions } from '@actions/exec/lib/interfaces';
 import { ParseOutputs, Outputs } from '../utils/utils';
-import { info, warning } from '@actions/core';
+import * as core from '@actions/core';
 
-export async function DeploySubscriptionScope(azPath: string, validationOnly: boolean, location: string, templateLocation: string, deploymentMode: string, deploymentName: string, parameters: string): Promise<Outputs> {
-    // Check if location is set
-    if (!location) {
-        throw Error("Location must be set.")
+export async function DeploySubscriptionScope(azPath: string, validationOnly: boolean, region: string, template: string, deploymentMode: string, deploymentName: string, parameters: string): Promise<Outputs> {
+    // Check if region is set
+    if (!region) {
+        throw Error("Region must be set.")
     }
 
     // check if mode is set as this will be ignored
     if (deploymentMode != "") {
-        warning("Deployment Mode is not supported for subscription scoped deployments, this parameter will be ignored!")
+        core.warning("Deployment Mode is not supported for subscription scoped deployments, this parameter will be ignored!")
     }
 
     // create the parameter list
     const azDeployParameters = [
-        location ? `--location ${location}` : undefined,
-        templateLocation ?
-            templateLocation.startsWith("http") ? `--template-uri ${templateLocation}` : `--template-file ${templateLocation}`
+        region ? `--location ${region}` : undefined,
+        template ?
+            template.startsWith("http") ? `--template-uri ${template}` : `--template-file ${template}`
             : undefined,
         deploymentName ? `--name ${deploymentName}` : undefined,
         parameters ? `--parameters ${parameters}` : undefined
@@ -31,7 +31,7 @@ export async function DeploySubscriptionScope(azPath: string, validationOnly: bo
         failOnStdErr: true,
         listeners: {
             stderr: (data: BufferSource) => {
-                warning(data.toString());
+                core.warning(data.toString());
             },
             stdline: (data: string) => {
                 if (!data.startsWith("[command]"))
@@ -45,25 +45,26 @@ export async function DeploySubscriptionScope(azPath: string, validationOnly: bo
         ignoreReturnCode: true,
         listeners: {
             stderr: (data: BufferSource) => {
-                warning(data.toString());
+                core.warning(data.toString());
             },
         }
     }
 
     // validate the deployment
-    info("Validating template...")
+    core.info("Validating template...")
     var code = await exec(`"${azPath}" deployment sub validate ${azDeployParameters} -o json`, [], validateOptions);
     if (validationOnly && code != 0) {
         throw new Error("Template validation failed")
     } else if (code != 0) {
-        warning("Template validation failed.")
+        core.warning("Template validation failed.")
     }
 
     // execute the deployment
-    info("Creating deployment...")
+    core.info("Creating deployment...")
     await exec(`"${azPath}" deployment sub create ${azDeployParameters} -o json`, [], deployOptions);
-
+    core.debug(commandOutput);
+    
     // Parse the Outputs
-    info("Parsing outputs...")
+    core.info("Parsing outputs...")
     return ParseOutputs(commandOutput)
 }
